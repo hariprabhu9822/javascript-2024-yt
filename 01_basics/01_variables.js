01d3459a0fb13f05bffbc3092dc3ef7675a6f050
hariprabhu9822 <48563917+hariprabhu9822@users.noreply.github.com>
2024-09-10 19:14:51 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,9 +1,16 @@
+// semicolons are not so imp in js 
+
 const accountId = 232342;
 // const variable value cannot be changed
 
 let accountEmail = "hari@gmail.com"
 var accountPassword = "12345"
 accountCity = "jaipur"
+
+/*If u dont give value to a variable that is u just declare it then it's value becomes undefined
+Eg : let account
+So value of account variable will be undefined */
+let accountState
 
 // accountId = 67
 // above line will give error saying "typeerror: Assignment to constant variable."
@@ -12,4 +19,8 @@ accountEmail = "vinil@gmail.com"
 accountPassword = "11111"
 accountCity = "margao"
 console.log(accountId)
-console.table([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+// dont use var in JS 
+// bcoz os issue in block scope and functional scope 
+// instead always use let
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" //treat all js code as newer version
+
+// alert(9+9) 
+// we are using node js and now browser. this will work on browser console but not here
+
+console.log(2) 
+console.log("hari")
+/*so instead of writing 2 console stmts on 1 line and putting semicolon in btw, we can writeit on 2 diff lines*/
+
+let name = "hari" //string
+let age = 69 //int
+let isLogged = true //boolean true/false
+let state //undefined
+let value = null //null -standalone value
+
+
+
+// number => 2 to power 53
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object 
+
+console.log(typeof "hie") //string
+console.log(typeof age) //number
+console.log(typeof null) //asked in interviews..type of null is object
+console.log(typeof undefined) //undefined
