890c75945dc81bb7624b00bfe321e1380636f3a3
hariprabhu9822 <48563917+hariprabhu9822@users.noreply.github.com>
2024-12-19 23:53:50 +0530

    Update test.js

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,7 +1,2 @@
 console.log("hari")
-// to run ths command ,press node 01_basics/test.js in terminal
-//xdxdxcdcxdxxd
-//nkjvvnkfvef
-//cvsfvdsfvsd
-//new
-sxscsddddddddddddddddddddddddddddddddddddddd
+
